011d513e5234271ef232439fd8d729aabd8aaf4e
ali bk <aliboukhalfa98@gmail.com>
2026-04-19 02:54:20 -0400

    Lancement de l interface graphique dans main.py

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from services.services_salle import ServiceSalle
-from models.salle import Salle
+from views.view_salle import ViewSalle
 
-service = ServiceSalle()
-
-print("Liste des salles :")
-for s in service.recuperer_salles():
-    s.afficher_infos()
-    print("------")
-
-salle1 = Salle("S101", "Salle A", "Classe", 20)
-ok, msg = service.ajouter_salle(salle1)
-print(msg)
-
-salle1.libelle = "Salle A modifiee"
-salle1.type = "Bureau"
-salle1.capacite = 15
-ok, msg = service.modifier_salle(salle1)
-print(msg)
-
-s = service.rechercher_salle("S101")
-if s:
-    print("Salle trouvée :")
-    s.afficher_infos()
-
-service.supprimer_salle("S101")
-print("Salle supprimée")
+app = ViewSalle()
+app.mainloop()
