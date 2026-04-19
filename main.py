694af9e66326baba965f6db36296f4fe7643f363
ali bk <aliboukhalfa98@gmail.com>
2026-04-19 01:19:47 -0400

    Ajout des tests de la couche Service dans main.py

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-from Data.dao_salle import DataSalle
+from services.services_salle import ServiceSalle
 from models.salle import Salle
 
-dao = DataSalle()
+service = ServiceSalle()
 
-# Test connexion
-try:
-    conn = dao.get_connection()
-    print("Connexion MySQL réussie")
-    conn.close()
-except Exception as e:
-    print("Erreur connexion :", e)
-
-# Test ajout
-s1 = Salle("S100", "Salle Test", "Classe", 25)
-dao.insert_salle(s1)
-print("Salle ajoutée")
-
-# Test recherche
-salle = dao.get_salle("S100")
-if salle:
-    print("Salle trouvée :")
-    salle.afficher_infos()
-
-# Test modification
-s1.libelle = "Salle modifiée"
-s1.type = "Laboratoire"
-s1.capacite = 30
-dao.update_salle(s1)
-print("Salle modifiée")
-
-# Test affichage toutes les salles
 print("Liste des salles :")
-liste = dao.get_salles()
-for s in liste:
+for s in service.recuperer_salles():
     s.afficher_infos()
     print("------")
 
-# Test suppression
-dao.delete_salle("S100")
+salle1 = Salle("S101", "Salle A", "Classe", 20)
+ok, msg = service.ajouter_salle(salle1)
+print(msg)
+
+salle1.libelle = "Salle A modifiee"
+salle1.type = "Bureau"
+salle1.capacite = 15
+ok, msg = service.modifier_salle(salle1)
+print(msg)
+
+s = service.rechercher_salle("S101")
+if s:
+    print("Salle trouvée :")
+    s.afficher_infos()
+
+service.supprimer_salle("S101")
 print("Salle supprimée")
